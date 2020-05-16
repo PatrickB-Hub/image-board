66669186d12a55eae866603f5b9de5759dc8b673
JayShiznit <12658037+PatrickB-Hub@users.noreply.github.com>
2020-05-16 15:32:16 +0100

    Added rsa-keypair generator

---
+++ server/src/generateKeypair.ts
@@ -0,0 +1,28 @@
+import crypto = require("crypto");
+import fs = require("fs");
+import path = require("path");
+
+/**
+ * This module will generate a public and private keypair and save them to the current directory
+ */
+
+function genKeyPair() {
+
+  const keyPair = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: "pkcs1",
+      format: "pem"
+    },
+    privateKeyEncoding: {
+      type: "pkcs1",
+      format: "pem"
+    },
+  });
+
+  fs.writeFileSync(path.join(__dirname, "id_rsa_pub.pem"), keyPair.publicKey);
+
+  fs.writeFileSync(path.join(__dirname, "id_rsa_priv.pem"), keyPair.privateKey);
+}
+
+genKeyPair();
