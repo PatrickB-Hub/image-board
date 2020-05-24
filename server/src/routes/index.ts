4177a05577f87e02808e7be091d2f3ffb5514967
JayShiznit <12658037+PatrickB-Hub@users.noreply.github.com>
2020-05-24 17:14:20 +0100

    Added 'add' route to posts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import users from "./users";
+import posts from "./posts";
 
 const router = Router();
 
 router.use("/api/users", users);
+router.use("/api/posts", posts);
 
 export default router;
