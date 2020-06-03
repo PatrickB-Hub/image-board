8d104daff8737e2a2a254a9a2c1cf6bb172bdda9
JayShiznit <12658037+PatrickB-Hub@users.noreply.github.com>
2020-06-03 17:37:11 +0100

    Add middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,27 @@
 import express from "express";
+import fileUpload from "express-fileupload";
+import passport from "passport";
+import dotenv from "dotenv";
+dotenv.config();
 
+import configure from "./config/passport";
+import router from "./routes/index";
 import "./config/database";
 
 const app = express();
+
+// allow files to be uploaded (limited to 5mb)
+app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
+
+configure(passport);
+
+app.use(passport.initialize());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(router);
+app.use('/static', express.static('public'))
 
 const port = 8080;
 app.listen(port, () => {
