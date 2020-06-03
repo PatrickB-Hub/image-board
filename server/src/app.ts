4f47d64ff75515cb0acd0f82f1791c8136aea6e5
JayShiznit <12658037+PatrickB-Hub@users.noreply.github.com>
2020-06-03 17:46:54 +0100

    Added docker compatibility

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,16 @@ import fileUpload from "express-fileupload";
 import passport from "passport";
 import dotenv from "dotenv";
 dotenv.config();
+import fs from "fs";
+import https from "https";
+import cors from "cors";
 
 import configure from "./config/passport";
 import router from "./routes/index";
 import "./config/database";
 
 const app = express();
+
 
 // allow files to be uploaded (limited to 5mb)
 app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
@@ -20,10 +24,30 @@ app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(router);
-app.use('/static', express.static('public'))
+app.use(cors());
 
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.use(router);
+app.use('/static', express.static('public'));
+
+// const port = 8085;
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+// OPTIONAL - SSL-Certificate
+const privateKey = fs.readFileSync('./privkey.pem', 'utf8');
+const certificate = fs.readFileSync('./cert.pem', 'utf8');
+const ca = fs.readFileSync('./chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+// OPTIONAL - only works with SSL-Certificate
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(443, () => {
+  console.log('Server running on port 443');
 });
