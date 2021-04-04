9484263a5508c2ca2389f1f1110fe4f87583313f
JayShiznit <12658037+PatrickB-Hub@users.noreply.github.com>
2021-04-04 14:43:08 +0200

    Updates

---
+++ precache-manifest.37fe65fc4538dcd0f2981ee4376d1a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259c8d5417d532eecec84d3e7513d847",
+    "url": "/image-board/index.html"
+  },
+  {
+    "revision": "c85102b3175ec634c623",
+    "url": "/image-board/static/css/main.391ebc07.chunk.css"
+  },
+  {
+    "revision": "9d79f5ae9b4b36e1ba95",
+    "url": "/image-board/static/js/2.b20a5618.chunk.js"
+  },
+  {
+    "revision": "4fb2edf6dacb8381c63ed96a8942acd3",
+    "url": "/image-board/static/js/2.b20a5618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85102b3175ec634c623",
+    "url": "/image-board/static/js/main.1f9a0094.chunk.js"
+  },
+  {
+    "revision": "d555c4a836f0237c7f25",
+    "url": "/image-board/static/js/runtime-main.b7a6c9be.js"
+  },
+  {
+    "revision": "bee8544aaaa32e6455abbce8fbcecbdb",
+    "url": "/image-board/static/media/cameras-background.bee8544a.png"
+  }
+]);
